6b3fc8e6e4495b9246a40ab78a2a642da4bd26d7
AeryIce <agahariiswarajati@gmail.com>
2025-12-03 15:38:30 +0700

    feat(api): stabilkan endpoint foundations & schools (list + detail slug) untuk direktori e-MPK (#2)
    * feat(foundation): add profile fields, Filament CRUD & API endpoints
    
    * feat(schools,foundations): sambungkan list & detail ke API Laravel + tambah loading state cloud

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FoundationController;
 
+use App\Http\Controllers\Api\SchoolController;
+
 Route::get('/foundations', [FoundationController::class, 'index']);
 Route::get('/foundations/{slug}', [FoundationController::class, 'show']);
+
+Route::get('/schools', [SchoolController::class, 'index']);
+Route::get('/schools/{slug}', [SchoolController::class, 'show']);
